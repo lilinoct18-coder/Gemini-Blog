c0387518c56afdbee88983ab766e38c08100fadf
Liling <lilin.oct18@gmail.com>
2026-02-06 12:22:50 +0800

    fix: Lock wave shape to prevent distortion during movement

--- src/components/WaveDivider.tsx
+++ src/components/WaveDivider.tsx
@@ -18,17 +18,9 @@ const WaveDivider: React.FC<{
         preserveAspectRatio="none"
         className="absolute h-full w-full"
       >
-        <motion.path
-          d="M50,0 C60,250 40,500 50,750 C60,1000 40,1250 50,1500 L0,1500 L0,0 Z"
+        <path
+          d="M50,0 C70,250 30,500 50,750 C70,1000 30,1250 50,1500 L0,1500 L0,0 Z"
           fill="#0a1628"
-          animate={{
-            d: hoverState === 'novis' 
-              ? "M70,0 C80,250 60,500 70,750 C80,1000 60,1250 70,1500 L0,1500 L0,0 Z"
-              : hoverState === 'liling'
-              ? "M30,0 C40,250 20,500 30,750 C40,1000 20,1250 30,1500 L0,1500 L0,0 Z"
-              : "M50,0 C60,250 40,500 50,750 C60,1000 40,1250 50,1500 L0,1500 L0,0 Z"
-          }}
-          transition={{ duration: 1.5, ease: [0.45, 0.05, 0.55, 0.95] }}
         />
       </svg>
       
